6262ae24e93c1a9d6061e9826b92864990a3906b
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2026-02-19 16:31:20 +0100

    feat(main): implemented tests automation.

--- main.py
+++ main.py
@@ -8,5 +8,15 @@
 
  Author : Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
  Date : 2026-02-17
- Version : 0.0.0
+ Version : 1.0.0
 """
+
+from unittest import main
+import medical_data_visualiser
+
+# Testing the function
+medical_data_visualiser.draw_cat_plot()
+medical_data_visualiser.draw_heat_map()
+
+# Running unit tests automatically
+main(module="test_module", exit=False)
