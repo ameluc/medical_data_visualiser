7edf96de0a481b07509e83221e929bce8450bd64
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2026-02-19 16:04:13 +0100

    feat(medical_data_visualiser.df): cleaned data to remove irrelevant ones.

--- medical_data_visualiser.py
+++ medical_data_visualiser.py
@@ -8,5 +8,16 @@
 
  Author : Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
  Date : 2026-02-17
- Version : 0.0.0
+ Version : 0.5.0
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+df: pd.DataFrame = pd.read_csv("medical_examination.csv")
+
+df["overweight"] = (df["weight"] / ((df["height"] /100)**2) > 25).astype(int)
+df["cholesterol"] = (df["cholesterol"]  > 1).astype(int)
+df["gluc"] = (df["gluc"]  > 1).astype(int)
